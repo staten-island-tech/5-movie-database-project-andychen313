fd285cbf687badff945a38ebf2440276a786292f
andychen313 <andyc313@nycstudents.net>
2025-10-21 21:32:07 -0400

    Update app.py

--- app.py
+++ app.py
@@ -14,8 +14,34 @@ title()
         print(index, item["year"])
 year() """
 
-def year():
+""" def year():
     year = input("Give me a year and I will print all movies released after that year: ")
     for index, item in enumerate(data):
         for year+1, 
-        print(index, item[""])
+        print(index, item[""]) """
+
+""" def titles(data):
+    for d in data:
+        print(d["title"])
+titles(data) """
+
+""" def after_year(data):
+    year = int(input("Give me a year and I will print all movies released after that year: "))
+    for d in data:
+        if int(d["year"]) > year:
+            print(f'{d["title"]} ({d["year"]})')
+after_year(data) """
+
+""" def before_year(data):
+    year = int(input("Give me a year and I will pritn all movies released before that year: "))
+    for d in data:
+        if int(d["year"]) < year:
+            print(f'{d["title"]} ({d["year"]})')
+before_year(data) """
+
+""" def during_year(data):
+    year = int(input("Give me a year and I will print all movies that were released in that year: "))
+    for d in data:
+        if d["year"] == year:
+            print(f'{d["title"]} ({d["year"]})')
+during_year(data) """
