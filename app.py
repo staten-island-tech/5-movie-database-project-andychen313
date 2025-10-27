f59f3a03a96fcf480b3883dfb1db0b62aef94c8f
andychen313 <andyc313@nycstudents.net>
2025-10-27 09:15:19 -0400

    Update app.py

--- app.py
+++ app.py
@@ -4,22 +4,6 @@ movies = open("./movies.json", encoding="utf8")
 ## create variable "data" that represents the enitre movie list
 data = json.load(movies)
 
-""" def title():
-    for index, item in enumerate(data):
-        print(index, item["title"])
-title()
- """
-""" def year():
-    for index, item in enumerate(data):
-        print(index, item["year"])
-year() """
-
-""" def year():
-    year = input("Give me a year and I will print all movies released after that year: ")
-    for index, item in enumerate(data):
-        for year+1, 
-        print(index, item[""]) """
-
 """ def titles(data):
     for d in data:
         print(d["title"])
@@ -45,3 +29,5 @@ before_year(data) """
         if d["year"] == year:
             print(f'{d["title"]} ({d["year"]})')
 during_year(data) """
+
+
