ffaba794db64902fbc09c7765c91a3896f395c2d
andychen313 <andyc313@nycstudents.net>
2025-10-28 09:25:49 -0400

    Update app.py

--- app.py
+++ app.py
@@ -4,10 +4,14 @@ movies = open("./movies.json", encoding="utf8")
 ## create variable "data" that represents the enitre movie list
 data = json.load(movies)
 
+#File One
+
 """ def titles(data):
     for d in data:
         print(d["title"])
 titles(data) """
+
+#File Two
 
 """ def after_year(data):
     year = int(input("Give me a year and I will print all movies released after that year: "))
@@ -16,12 +20,16 @@ titles(data) """
             print(f'{d["title"]} ({d["year"]})')
 after_year(data) """
 
+#File Three
+
 """ def before_year(data):
     year = int(input("Give me a year and I will pritn all movies released before that year: "))
     for d in data:
         if int(d["year"]) < year:
             print(f'{d["title"]} ({d["year"]})')
 before_year(data) """
+
+#File Four
 
 """ def during_year(data):
     year = int(input("Give me a year and I will print all movies that were released in that year: "))
@@ -30,4 +38,21 @@ before_year(data) """
             print(f'{d["title"]} ({d["year"]})')
 during_year(data) """
 
+#File Five
+
+def search(data):
+    find = input("What movie(s) are you searching for?: ")
+    title = [data['title']]
+    for movie in data:
+        if find == title:
+            print(title)
+search(data)
+
+#File Six
+
+""" def genre_search(data):
+    genre_list = [data["genres"]]
+    movie = input("Give me a genre to search for movies of the same genre: ")
+    for movie in data:
+        for movie in genre  """
 
