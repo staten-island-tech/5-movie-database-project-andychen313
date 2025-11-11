e83bf4d7a3cae5480a3cdd707d3d188e263e97b8
andychen313 <andyc313@nycstudents.net>
2025-11-11 11:41:17 -0500

    Update app.py

--- app.py
+++ app.py
@@ -26,26 +26,7 @@ titles(data) """
 after_year(data) """
 
 #File Three
-
 """ def before_and_after_year(data):
-    years = [int(d["year"]) for d in data]
-    year_start = input("Enter the year you want your movies to be released after: ")
-    year_end = input("Enter the year you want your movies to be released before: ")
-    while (not year_start.isnumeric() or not year_end.isnumeric()) or (int(year_start) not in years or int(year_end) not in years):
-        print("Invalid year(s). Please enter an appropriate year that accomodates the movie.")
-        year_start = input("Enter the year you want your movies to be released after: ")
-        year_end = input("Enter the year you want your movies to be released before: ")
-    year_start = int(year_start)
-    year_end = int(year_end)
-    matching_movies = [d for d in data if year_start < int(d["year"]) < year_end]
-    if matching_movies:
-        for d in matching_movies:
-            print(f"{d['title']} ({d['year']})")
-    else:
-        print("No movies found.")
-before_and_after_year(data)
- """
-def before_and_after_year(data):
     years = [int(d["year"]) for d in data]
     
     year_start = input("Enter the year you wnat your movies to be released after: ")
@@ -72,32 +53,67 @@ def before_and_after_year(data):
             print(f"{d['title']} ({d['year']})")
     else:
         print("No movies found.")
-before_and_after_year(data)
+before_and_after_year(data) """
+
 #File Four
 
 """ def during_year(data):
-    year = int(input("Give me a year and I will print all movies that were released in that year: "))
-    for d in data:
-        if d["year"] == year:
+    years = [int(d['year']) for d in data]
+    year = input("Enter a year and I will print all movies that were released in that year: ")
+    while True:
+        if not year.isnumeric():
+            print("Invalid year. Please enter an appropriate year that you want your movie(s) to accomodate.")
+        elif int(year) not in years:
+            print("Invalid year. Please enter an appropriate year that you want your movie(s) to accomodate.")
+        else:
+            break
+        year = input("Enter a year and I will print all movies that were released in that year: ")
+    year = int(year)
+
+    matching_movies = [d for d in data if year == int(d["year"])]
+
+    if matching_movies:
+        for d in matching_movies:
             print(f'{d["title"]} ({d["year"]})')
-during_year(data)
- """
+    else:
+        print("No movies found")
+during_year(data) """
+
 #File Five
 
-""" def search_movies():
-    search = input("What movie are you trying to find?: ").lower()
-    movie_results = [d for d in data if search in d["title"].lower()]
-    print(movie_results)
-search_movies() """
+""" def search_movies(title):
+    results = []
+    for d in data:
+        if title.lower() in d["title"].lower():
+            results.append(d)
+    return results
+
+search = input("What movie are you trying to find?: ").lower()
+matches = search_movies(search)
+
+if matches:
+    for m in matches:
+        print(f"{m['title']} ({m['year']}) - ({m['genres']})")
+else:
+    print("No movies found.") """
 
 #File Six
+""" def genre_search(genre):
+    results = []
+    for d in data:
+        if any(genre.lower() in g.lower() for g in d["genres"]):
+            results.append(d)
+    return results
+    
+search = input("What genre of movies are you trying to find?: ").lower()
+matches = genre_search(search)
 
-""" def genre_search():
-    search = input("What genre of movies are you trying to find?: ").lower()
-    movie_results = [d for d in data if any(search in g.lower() for g in d["genres"])]
-    print(movie_results)
-genre_search() """
-
+if matches:
+    for m in matches:
+        print(f"{m['title']} ({m['year']}) - ({m['genres']})")
+else:
+    print("No movies found. Please try again using the full word(s) of the genres your looking for.")
+ """
 #Practice
 
 """ def titles(data):
